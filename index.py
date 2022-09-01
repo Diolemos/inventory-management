3ce232fdeb5a7613c55bb0a8da96cbf1e5fef05f
Dio Lemos <pedrodiogenes@outlook.com>
2022-09-01 15:43:47 -0300

    define what the program functions will be

---
+++ index.py
@@ -0,0 +1,28 @@
+products = [{"name":"water bottle",
+ "manufacturer": "Tupperware", "code": 1375 },{"name":"Accustic guitar",
+ "manufacturer": "Yamaha", "code": 1387 }, {"name":"Piano",
+ "manufacturer": "Yamaha", "code": 1388 },{"name":"pendrive",
+ "manufacturer": "sandisk", "code": 1466 }]
+
+
+
+
+def registerProduct():
+     print("test")
+     #whats the product's name?
+     #what's its manufacturer?
+     #what's its cost ? 
+     #save it to a dictionary 
+
+
+def consultProduct():
+    #sub menu : consult all products
+                #consult by code
+                #consult by manufacturer
+                #back to main manu
+                
+
+
+def removeProduct():
+             #ask for the product's code
+                         
