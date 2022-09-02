2f4ceecc064bae565224f5d26ae4ec3b16734425
Dio Lemos <pedrodiogenes@outlook.com>
2022-09-02 01:52:22 -0300

    update the register item function

--- index.py
+++ index.py
@@ -1,18 +1,21 @@
 products = [{"name":"water bottle",
- "manufacturer": "Tupperware", "code": 1375 },{"name":"Accustic guitar",
- "manufacturer": "Yamaha", "code": 1387 }, {"name":"Piano",
- "manufacturer": "Yamaha", "code": 1388 },{"name":"pendrive",
- "manufacturer": "sandisk", "code": 1466 }]
+ "manufacturer": "Tupperware", "code": 1 },{"name":"Accustic guitar",
+ "manufacturer": "Yamaha", "code": 2}, {"name":"Piano",
+ "manufacturer": "Yamaha", "code": 3 },{"name":"pendrive",
+ "manufacturer": "sandisk", "code": 4 }]
+
+codeCounter = 5 #counter for tracking the product codes
 
 
-
-
-def registerProduct():
-     print("test")
-     #whats the product's name?
-     #what's its manufacturer?
+def registerProduct(code):
+     name = input("What's the product name?")  #whats the product's name?
+   
+     manufacturer = input("What's the manufacturer?")#what's its manufacturer?
      #what's its cost ? 
      #save it to a dictionary 
+     newProduct = {"name":name,"manufacturer": manufacturer,"code": code}  
+     codeCounter += 1
+     list.append(newProduct)
 
 
 def consultProduct():
@@ -26,8 +29,9 @@ def consultProduct():
 def removeProduct():
              #ask for the product's code
 
+
 while True:
-    print("Welcom the stock managment system")
+    print("Welcom the Pedro Diogenes' stock managment system")
     print("What would you like to do?")
     print("1 - Register new product")
     print("2 - consult product")
@@ -37,16 +41,19 @@ while True:
     choice = int(input(""))
     
     if choice == 1:
-        registerProduct()
+        print("You chose 1- Register new product")
+        registerProduct(codeCounter)
         continue
     elif choice == 2:
+        print("You chose 2- Consult product")
         consultProduct()
         continue
     elif choice == 3:
+        print("You chose 3- remove a product")
         removeProduct()
         continue
     elif choice == 4:
-        print("exeting...")
+        print("You chose- 4, exeting...")
         break    
     else:
         print("Error. invelid option")
